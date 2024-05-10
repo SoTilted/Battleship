e5c8c447edf68d37f124bdbeeefd637c59975d1c
SoTilted <103329657+SoTilted@users.noreply.github.com>
2024-05-10 14:41:13 +0300

    Change test file name

---
+++ classes.test.js
@@ -0,0 +1,51 @@
+import { Ship, Gameboard, Player } from "./classes";
+
+it("Tests if  ship has been hit.", () => {
+  const ship1 = new Ship(5);
+  ship1.hit();
+  expect(ship1.hits).toBe(1);
+});
+
+it("Tests if ship has been sunk.", () => {
+  const ship2 = new Ship(2);
+  ship2.hit();
+  ship2.hit();
+  ship2.isSunk();
+  expect(ship2.sunk).toBe(true);
+});
+
+it("Tests if ship has been placed.", () => {
+  const ship3 = new Ship(3);
+  const Gameboard1 = new Gameboard();
+  Gameboard1.placeShip(ship3, ["A1", "A2", "A3"]);
+  expect(Gameboard1.myBoard).toStrictEqual({
+    A: [ship3, ship3, ship3, "", "", "", "", "", "", ""],
+    B: ["", "", "", "", "", "", "", "", "", ""],
+    C: ["", "", "", "", "", "", "", "", "", ""],
+    D: ["", "", "", "", "", "", "", "", "", ""],
+    E: ["", "", "", "", "", "", "", "", "", ""],
+    F: ["", "", "", "", "", "", "", "", "", ""],
+    G: ["", "", "", "", "", "", "", "", "", ""],
+    H: ["", "", "", "", "", "", "", "", "", ""],
+    I: ["", "", "", "", "", "", "", "", "", ""],
+    J: ["", "", "", "", "", "", "", "", "", ""],
+  });
+});
+
+it("Tests if received attack is registered correctly.", () => {
+    const ship4 = new Ship(2);
+    const Gameboard2 = new Gameboard();
+    Gameboard2.placeShip(ship4,['A1','A2'])
+    Gameboard2.receiveAttack('A',2);
+    expect(ship4.hits).toBe(1);
+});
+
+it ("Tests if players are created correctly.",()=>{
+    const player1 = new Player('real');
+    const player2 = new Player('computer');
+    player1.name ='Jerry';
+    player1.playerGameBoard.placeShip(player1.ships['Carrier'],['A1','A2','A3','A4','A5',]);
+    player1.playerGameBoard.receiveAttack('A',1);
+    expect(player1.name).toBe('Jerry');
+    expect(player1.ships['Carrier'].hits).toBe(1)
+})
