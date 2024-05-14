7eaaa5140c3ab4dbf4ab31dc1a846ea274e4bb74
SoTilted <103329657+SoTilted@users.noreply.github.com>
2024-05-14 14:37:31 +0300

    Seperate drag and drop to a specific file

--- src/DragAndDrop.js
+++ src/DragAndDrop.js
@@ -1,6 +1,30 @@
 // drag and drop
 let pointOfContact = 0;
 let placedShips = [];
+const content = document.querySelector(".content");
+const header = document.querySelector(".header");
+const footer = document.querySelector(".footer");
+
+content.addEventListener("dragend", mouseOutOfBounds);
+header.addEventListener("dragend", mouseOutOfBounds);
+footer.addEventListener("dragend", mouseOutOfBounds);
+
+function mouseOutOfBounds() {
+  // When a ship is dragged over the grid and out of bounds, it clears
+  // the classes so the grid doesn't stay wrongly coloured.
+  const allCells = document.querySelectorAll(".cell");
+  if (allCells !== null) {
+    allCells.forEach((element) => {
+      let classList = [...element.classList];
+      if (classList.includes("draggedOverNegative")) {
+        element.classList.remove("draggedOverNegative");
+      }
+      if (classList.includes("draggedOver")) {
+        element.classList.remove("draggedOver");
+      }
+    });
+  }
+}
 function allowDrop(ev) {
   // Responsible of coloring the grid on dragover depending on the leggallity of the move.
   ev.preventDefault();
--- src/index.js
+++ src/index.js
@@ -17,27 +17,4 @@ startNewGameButton.addEventListener("click", (e) => {
   document.querySelector("dialog").close();
 });
 
-const content = document.querySelector(".content");
-const header = document.querySelector(".header");
-const footer = document.querySelector(".footer");
 
-content.addEventListener("dragend", mouseOutOfBounds);
-header.addEventListener("dragend", mouseOutOfBounds);
-footer.addEventListener("dragend", mouseOutOfBounds);
-
-function mouseOutOfBounds() {
-  // When a ship is dragged over the grid and out of bounds, it clears
-  // the classes so the grid doesn't stay wrongly coloured.
-  const allCells = document.querySelectorAll(".cell");
-  if (allCells !== null) {
-    allCells.forEach((element) => {
-      let classList = [...element.classList];
-      if (classList.includes("draggedOverNegative")) {
-        element.classList.remove("draggedOverNegative");
-      }
-      if (classList.includes("draggedOver")) {
-        element.classList.remove("draggedOver");
-      }
-    });
-  }
-}
